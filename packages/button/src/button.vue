3829b340f0a37c31f4743bdf6acb20d4a4538d17
wsh <675580180@qq.com>
2019-09-09 16:19:52 +0800

    [feature]: 提取css

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -37,23 +37,5 @@ export default {
 </script>
 
 <style>
-  .wsh-button{display: inline-block;line-height: 1;white-space: nowrap;cursor: pointer;background: #fff;
-    border: 1px solid #dcdfe6;color: #606266;-webkit-appearance: none;text-align: center;
-    box-sizing: border-box;outline: none;margin: 0;transition: .1s;font-weight: 500;
-    -moz-user-select: none;-webkit-user-select: none;-ms-user-select: none;
-    padding: 12px 20px;font-size: 14px;border-radius: 4px;}
-  .wsh-button--small{padding: 9px 15px;}
-  .wsh-button--medium{padding: 10px 20px;}
-
-  .wsh-button--success{color: #fff;background-color: #67c23a;border-color: #67c23a;}
-  .wsh-button--danger{color: #fff;background-color: #f56c6c;border-color: #f56c6c;}
-
-  .wsh-button:focus,.wsh-button:hover{color:#409EFF;border-color:#c6e2ff;background-color:#ecf5ff}
-  .wsh-button:active{color:#3a8ee6;border-color:#3a8ee6;outline:0}
-  .wsh-button--success:focus,.wsh-button--success:hover{background:#85ce61;border-color:#85ce61;color:#FFF}
-  .wsh-button--success.is-active,.wsh-button--success:active{background:#5daf34;border-color:#5daf34;color:#FFF}
-  .wsh-button--success:active{outline:0}
-  .wsh-button--danger:focus,.wsh-button--danger:hover{background:#f78989;border-color:#f78989;color:#FFF}
-  .wsh-button--danger.is-active,.wsh-button--danger:active{background:#dd6161;border-color:#dd6161;color:#FFF}
-  .wsh-button--danger:active{outline:0}
+  @import '../../../lib/theme-chalk/button.css';
 </style>
--- packages/input/src/input.vue
+++ packages/input/src/input.vue
@@ -7,7 +7,7 @@
     <textarea
     v-else
     class="wsh-textarea__inner">
-    </textareav-else>
+    </textarea>
   </div>
 </template>
 
@@ -45,19 +45,5 @@ export default {
 </script>
 
 <style>
-  .wsh-input{cursor: pointer;position: relative;font-size: 14px;display: inline-block;width: 100%;}
-  .wsh-input__inner{cursor: pointer;-webkit-appearance: none;background-color: #fff;background-image: none;
-    border-radius: 4px;border: 1px solid #dcdfe6;box-sizing: border-box;color: #606266;
-    display: inline-block;font-size: inherit;height: 40px;line-height: 40px;outline: none;
-    padding: 0 15px;transition: border-color .2s cubic-bezier(.645,.045,.355,1);width: 100%;}
-  .wsh-input--medium .wsh-input__inner {height: 36px;line-height: 36px;}
-  .wsh-input--small .wsh-input__inner {height: 32px;line-height: 32px;}
-  .wsh-input.is-active .wsh-input__inner,.wsh-input__inner:focus{border-color:#409EFF;outline:0}
-
-  .wsh-textarea {position: relative;display: inline-block;width: 100%;vertical-align: bottom;font-size: 14px;}
-  .wsh-textarea__inner {display: block;resize: vertical;padding: 5px 15px;line-height: 1.5;
-    box-sizing: border-box;width: 100%;font-size: inherit;color: #606266;
-    background-color: #fff;background-image: none;border: 1px solid #dcdfe6;
-    border-radius: 4px;transition: border-color .2s cubic-bezier(.645,.045,.355,1);}
-  .wsh-textarea__inner:focus{outline:0;border-color:#409EFF}
+  @import '../../../lib/theme-chalk/input.css';
 </style>
